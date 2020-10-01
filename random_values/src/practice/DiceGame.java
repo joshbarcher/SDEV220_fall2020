2d016380e4e77a6d47e1e2563434177db678870d
Josh Archer <jarcher@greenriver.edu>
2020-10-01 10:14:19 -0700

    Added a solution for our in-class exercise.

---
+++ random_values/src/practice/DiceGame.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.Random;
+
+public class DiceGame
+{
+    private static Random random = new Random();
+
+    public static void main(String[] args)
+    {
+        System.out.println("Rolling fair die 10 times");
+        System.out.println("*************************");
+        for (int i = 1; i <= 10; i++)
+        {
+            if (random.nextBoolean())
+            {
+                System.out.println("Rolled a 1");
+            }
+            else
+            {
+                System.out.println("Rolled a " + nextInt(2, 6));
+            }
+        }
+    }
+
+    public static int nextInt(int low, int high)
+    {
+        return low + random.nextInt(high - low + 1);
+    }
+}
