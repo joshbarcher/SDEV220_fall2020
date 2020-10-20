b15da96c58c0d92d12e9f2e0ce27c5853f1182bf
Josh Archer <jarcher@greenriver.edu>
2020-10-20 10:51:20 -0700

    Added some examples of Java generics.

--- generics/src/exercise/TestPairs.java
+++ generics/src/exercise/TestPairs.java
@@ -4,8 +4,22 @@ public class TestPairs
 {
     public static void main(String[] args)
     {
+        //creating test Pair objects
         Pair<Boolean, String> pair = new Pair<>(true, "awesome");
         System.out.println(pair.getFirst());
         System.out.println(pair.getSecond().toUpperCase());
+
+        //test your methods below...
+
+    }
+
+    public static Pair<Integer, Integer> divide(int num, int den)
+    {
+        return null;
+    }
+
+    public static String joinTogether(Pair<String, String> stringPair)
+    {
+        return null;
     }
 }
