dfb99cc3619491f42cb66b003f2dfb85f1c1fa73
Josh Archer <jarcher@greenriver.edu>
2020-11-20 10:35:11 -0800

    Added an example of a singly linked list.

---
+++ linked_list/src/singly_linked/TestingOurLL.java
@@ -0,0 +1,14 @@
+package singly_linked;
+
+public class TestingOurLL
+{
+    public static void main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+
+        list.add('A');
+        list.add('B');
+        list.add('C');
+        list.add('D');
+    }
+}
